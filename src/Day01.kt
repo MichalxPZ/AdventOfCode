dc272f27a3d11408ce65d2b4c94fa03b1d52f217
MichalxPZ <michauz2001@gmail.com>
2021-12-01 20:31:03 +0100

    day01

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,37 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        var increases = 0;
+        val data = input.map { it.toInt() }
+        var measurement = data.first()
+        for (i in 1..data.size - 1) {
+            if (data.get(i) > measurement) {
+                increases += 1;
+            }
+            measurement = data.get(i)
+        }
+        return increases
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val data = input.map { it.toInt() }
+        val sums = data.windowed(3, 1)
+                .toList()
+                .map { it.sum() }
+        var measurement = sums.first()
+        var increases = 0;
+        for (i in 1..sums.size-1) {
+            if (sums.get(i) > measurement) {
+                increases += 1;
+            }
+            measurement = sums.get(i)
+        }
+        return increases
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 7)
+    check(part2(testInput) == 5)
 
     val input = readInput("Day01")
     println(part1(input))
